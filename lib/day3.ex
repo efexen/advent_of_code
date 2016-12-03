c93c91779be51fe16466c8baab73a38a3caa2538
efexen <fxn@fxndev.com>
2016-12-03 17:31:07 +0000

    Day 3 part 2

--- lib/day3.ex
+++ lib/day3.ex
@@ -7,6 +7,15 @@ defmodule Day3 do
     |> Enum.count
   end
 
+  def solve_columns(path) do
+    File.stream!(path)
+    |> Stream.map(&clean/1)
+    |> Stream.chunk(3)
+    |> Stream.flat_map(&transpose/1)
+    |> Stream.filter(&validate/1)
+    |> Enum.count
+  end
+
   defp clean(lengths) do
     lengths
     |> String.trim
@@ -16,6 +25,12 @@ defmodule Day3 do
 
   defp validate(lengths) do
     2 * Enum.max(lengths) < Enum.sum(lengths)
+  end
+
+  defp transpose(lists) do
+    lists
+    |> List.zip
+    |> Enum.map(&Tuple.to_list/1)
   end
 
 end
--- test/day3_test.exs
+++ test/day3_test.exs
@@ -4,4 +4,9 @@ defmodule Day3Test do
   test "Solves part 1 example" do
     assert Day3.solve("test/fixtures/day3_example.txt") == 1
   end
+
+  test "Solves part 2 example" do
+    assert Day3.solve_columns("test/fixtures/day3_example.txt") == 2
+  end
+
 end
