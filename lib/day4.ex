1b01d92392b4d9f4b4fa77f69ffdff3efc3a3dfa
efexen <fxn@fxndev.com>
2016-12-04 13:25:28 +0000

    Day4 part 2

--- lib/day4.ex
+++ lib/day4.ex
@@ -4,17 +4,28 @@ defmodule Day4 do
     File.stream!(path)
     |> Stream.map(&clean/1)
     |> Stream.filter(&validate/1)
-    |> Stream.map(fn ([_, id, _]) -> String.to_integer(id) end)
+    |> Stream.map(fn ([_, _, id, _]) -> String.to_integer(id) end)
     |> Enum.sum
   end
 
-  defp clean(room) do
-    Regex.run(~r/(?<code>[a-z|-]+)-(?<id>\d+)\[(?<checksum>[a-z]+)\]/, room)
-    |> Enum.map(&(String.replace(&1, "-", "")))
-    |> List.delete_at(0)
+  def solve2(path) do
+    File.stream!(path)
+    |> Stream.map(&clean/1)
+    |> Stream.filter(&validate/1)
+    |> Stream.map(&decrypt/1)
+    |> Enum.into([])
   end
 
-  defp validate([code, id, checksum]) do
+  defp clean(room) do
+    matches = Regex.run(~r/(?<code>[a-z|-]+)-(?<id>\d+)\[(?<checksum>[a-z]+)\]/, room)
+    original = Enum.at(matches, 1)
+
+    matches
+    |> Enum.map(&(String.replace(&1, "-", "")))
+    |> List.replace_at(0, original)
+  end
+
+  defp validate([_encrypted, code, _id, checksum]) do
     checksum == code
     |> String.graphemes
     |> Enum.reduce(%{}, &count_char/2)
@@ -34,6 +45,22 @@ defmodule Day4 do
 
   defp count_char(char, map) do
     Map.update(map, char, 1, &(&1 + 1))
+  end
+
+  defp decrypt([encrypted, _, id, _]) do
+    offset = id |> String.to_integer |> rem(26)
+
+    decrypted = encrypted
+    |> String.graphemes
+    |> Enum.map(&(decrypt_char(&1, offset)))
+    |> Enum.join
+
+    { decrypted, id }
+  end
+
+  defp decrypt_char("-", _), do: " "
+  defp decrypt_char(<<letter>>, offset) do
+    <<?a + rem((letter - ?a) + offset, 26)>>
   end
 
 end
--- test/day4_test.exs
+++ test/day4_test.exs
@@ -5,4 +5,8 @@ defmodule Day4Test do
     assert Day4.solve("test/fixtures/day4_example.txt") == 1514
   end
 
+  test "Solve part 2" do
+    assert Day4.solve2("test/fixtures/day4_2_example.txt") == [{"very encrypted name", 343}]
+  end
+
 end
