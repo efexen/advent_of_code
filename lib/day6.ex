7f2c915066a9fc17ec12b9ca3585e72818d9c1ac
efexen <fxn@fxndev.com>
2016-12-06 20:06:23 +0000

    Day6 part 2

--- lib/day6.ex
+++ lib/day6.ex
@@ -1,17 +1,31 @@
 defmodule Day6 do
 
   def solve(path) do
+    do_solve(path, &pick_top/1)
+  end
+
+  def solve2(path) do
+    do_solve(path, &pick_bottom/1)
+  end
+
+  def do_solve(path, picker) do
     File.stream!(path)
     |> Stream.map(&String.trim/1)
     |> Stream.map(&String.graphemes/1)
     |> Enum.reduce(%{}, &process_row/2)
-    |> Enum.map(&pick_top/1)
+    |> Enum.map(picker)
     |> Enum.join
   end
 
   defp pick_top({_, letters}) do
     letters
     |> Enum.max_by(&(elem(&1, 1)))
+    |> elem(0)
+  end
+
+  defp pick_bottom({_, letters}) do
+    letters
+    |> Enum.min_by(&(elem(&1, 1)))
     |> elem(0)
   end
 
--- test/day6_test.exs
+++ test/day6_test.exs
@@ -5,4 +5,8 @@ defmodule Day6Test do
     assert Day6.solve("test/fixtures/day6_example.txt") == "easter"
   end
 
+  test "Solves part 2 example" do
+    assert Day6.solve2("test/fixtures/day6_example.txt") == "advent"
+  end
+
 end
