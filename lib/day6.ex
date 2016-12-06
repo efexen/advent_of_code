81c08a4947fb2173ecf0644a590c8fa2ede19022
efexen <fxn@fxndev.com>
2016-12-06 19:59:37 +0000

    Day6 part 1

---
+++ lib/day6.ex
@@ -0,0 +1,33 @@
+defmodule Day6 do
+
+  def solve(path) do
+    File.stream!(path)
+    |> Stream.map(&String.trim/1)
+    |> Stream.map(&String.graphemes/1)
+    |> Enum.reduce(%{}, &process_row/2)
+    |> Enum.map(&pick_top/1)
+    |> Enum.join
+  end
+
+  defp pick_top({_, letters}) do
+    letters
+    |> Enum.max_by(&(elem(&1, 1)))
+    |> elem(0)
+  end
+
+  defp process_row(row, chars) do
+    row
+    |> Enum.with_index
+    |> Enum.reduce(chars, &count_char/2)
+  end
+
+  defp count_char({char, index}, map) do
+    Map.get_and_update(map, index, fn
+      index_map = %{} ->
+        { index_map, Map.update(index_map, char, 1, &(&1 + 1)) }
+      nil ->
+        { nil, %{ char => 1 } }
+    end) |> elem(1)
+  end
+
+end
---
+++ test/day6_test.exs
@@ -0,0 +1,8 @@
+defmodule Day6Test do
+  use ExUnit.Case
+
+  test "Solves part 1 example" do
+    assert Day6.solve("test/fixtures/day6_example.txt") == "easter"
+  end
+
+end
